4ca1327beb2cb91fd57d95ac66b474ab80bbf60b
Abdelaziz Salah <abdelaziz132001@gmail.com>
2025-11-10 22:57:11 +0100

    sync

--- Abdelaziz_Codes/Sheet1_codes/task2_script.py
+++ Abdelaziz_Codes/Sheet1_codes/task2_script.py
@@ -511,7 +511,7 @@ def task2e_optimized(packetsPathNpy = "all_packets.npy", packetListPathPcap='../
     # Generate all packet pairs
 
     print('computing all distances now')
-    return compute_all_distances(all_packets_array, label)
+    return compute_chebyshev_blockwise_parallel(all_packets_array, label)
     
 
 
