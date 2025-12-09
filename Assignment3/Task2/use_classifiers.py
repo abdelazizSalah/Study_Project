51c481ad94ed7a66676b6de2b75edfbc2fc68715
Abdelaziz Salah <abdelaziz132001@gmail.com>
2025-12-09 22:10:26 +0100

    trying to run  on re

--- Assignment3/Task2/use_classifiers.py
+++ Assignment3/Task2/use_classifiers.py
@@ -129,12 +129,12 @@ def execute_fold_for_experiments(fold_idx, binary_numeric_labels, timestamps,
     # Todo: here I can decrease the number of the training data.
     print('working on small portion of the data only')
     # get 1000 normal and 1,000 attack samples
-    normal_indices = np.where(y_train == 0)[0][:1000]
-    attack_indices = np.where(y_train == 1)[0][:1000]
+    normal_indices = np.where(y_train == 0)[0][:500]
+    attack_indices = np.where(y_train == 1)[0][:500]
     selected_indices = np.concatenate([normal_indices, attack_indices])
     X_train = X_train[selected_indices]
     y_train = y_train[selected_indices]
-    
+
 
     # print unique labels
     print("Unique labels in y_train:", np.unique(y_train))
@@ -351,7 +351,7 @@ def execute_experiments_def(global_label_encoder, k, param=5):
     for clf_name in classifiers_to_run:
         scenarios_for_clf = valid_scenarios_for(clf_name)
         for scen in scenarios_for_clf:
-            run_one(clf_name, scen)
+            run_one(clf_name, scen,param=param)
 
 
     return
