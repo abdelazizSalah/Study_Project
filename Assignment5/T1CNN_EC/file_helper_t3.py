2e62a84ec2f3bccd378e2dde7c145083b520657f
dW5kZWFk <an-dembowski@t-online.de>
2026-01-09 23:06:41 +0100

    fix required files check

--- Assignment5/T1CNN_EC/file_helper_t3.py
+++ Assignment5/T1CNN_EC/file_helper_t3.py
@@ -67,11 +67,11 @@ def verify_amount_feature_files(k: int, dataset_dir: str = "datasets") -> int:
     """
     missing = []
 
-    for prefix in ("raw"):
-        for fold_idx in range(k):
-            expected_file = os.path.join(dataset_dir, f"{prefix}_features_fold{fold_idx}.npy")
-            if not os.path.exists(expected_file):
-                missing.append(expected_file)
+    prefix="raw"
+    for fold_idx in range(k):
+        expected_file = os.path.join(dataset_dir, f"{prefix}_features_fold{fold_idx}.npy")
+        if not os.path.exists(expected_file):
+            missing.append(expected_file)
 
     if missing:
         print("\n[ERROR] Missing feature files:")
